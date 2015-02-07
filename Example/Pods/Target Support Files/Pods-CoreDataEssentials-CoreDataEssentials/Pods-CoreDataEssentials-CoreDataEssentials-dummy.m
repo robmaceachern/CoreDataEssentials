4d150d571aaddbcf5118c112fc041c4346983577
Rob MacEachern <rob@robmaceachern.com>
2015-02-06 23:49:20 -0800

    Pod lib create

---
+++ Example/Pods/Target Support Files/Pods-CoreDataEssentials-CoreDataEssentials/Pods-CoreDataEssentials-CoreDataEssentials-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CoreDataEssentials_CoreDataEssentials : NSObject
+@end
+@implementation PodsDummy_Pods_CoreDataEssentials_CoreDataEssentials
+@end
---
+++ Example/Pods/Target Support Files/Pods-CoreDataEssentials/Pods-CoreDataEssentials-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// CoreDataEssentials
+#define COCOAPODS_POD_AVAILABLE_CoreDataEssentials
+#define COCOAPODS_VERSION_MAJOR_CoreDataEssentials 0
+#define COCOAPODS_VERSION_MINOR_CoreDataEssentials 1
+#define COCOAPODS_VERSION_PATCH_CoreDataEssentials 0
+
